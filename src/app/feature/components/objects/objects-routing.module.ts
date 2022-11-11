eeefc74187632802788bbf5ec4f7350cbb869ede
brestradapoligran <brestrada@poligran.edu.co>
2022-11-11 14:48:27 -0500

    Feature: Objetos

--- src/app/feature/components/objects/objects-routing.module.ts
+++ src/app/feature/components/objects/objects-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateObjectComponent } from './components/create-object/create-object.component';
 import { ListComponent } from './components/list/list.component';
 
 const routes: Routes = [
   {
     path: '', component: ListComponent
+  },
+  {
+    path: 'create', component: CreateObjectComponent
   }
 ];
 
--- src/app/feature/components/objects/objects.module.ts
+++ src/app/feature/components/objects/objects.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ObjectsRoutingModule } from './objects-routing.module';
 import { ListComponent } from './components/list/list.component';
-import { FilterComponent } from './components/filter/filter.component'
+import { FilterComponent } from './components/filter/filter.component';
+import { CreateObjectComponent } from './components/create-object/create-object.component'
 
 
 @NgModule({
   declarations: [
     ListComponent,
-    FilterComponent
+    FilterComponent,
+    CreateObjectComponent
   ],
   imports: [
     CommonModule,
