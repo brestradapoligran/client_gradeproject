20e673b7400889e61d86c543ef22d50b77a6050f
brestradapoligran <brestrada@poligran.edu.co>
2023-01-02 17:25:39 -0500

    Feature: se agrega mi perfil y editar mi perfil

--- src/app/feature/components/users/createuser/createuser.component.ts
+++ src/app/feature/components/users/createuser/createuser.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
 import { UserRoleEnum } from 'src/app/feature/models/enum/UserRolesEnum';
 import { UserStatusEnum } from 'src/app/feature/models/enum/UserStatusEnum';
+import { EventTypes } from 'src/app/feature/models/toast/event-types';
 import UserModel from 'src/app/feature/models/user.model';
 import { ApiService } from 'src/app/feature/services/api/api.service';
+import { ToastService } from 'src/app/feature/services/toast/toast.service';
 import { ApiMethods } from 'src/app/feature/utils/api-methods';
 
 @Component({
@@ -14,7 +17,7 @@ import { ApiMethods } from 'src/app/feature/utils/api-methods';
 })
 export class CreateuserComponent implements OnInit {
 
-  title: String = "Crear usuario";
+  title: String = 'Crear Usuario';
   id: String = "";
   user: any;
   roles: string[] = Object.keys(UserRoleEnum).filter((item) => isNaN(Number(item)));
@@ -31,11 +34,16 @@ export class CreateuserComponent implements OnInit {
     status: new FormControl('')
   });
 
-  constructor(private api: ApiService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private api: ApiService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private toastService: ToastService) { }
 
   ngOnInit(): void {
-    this.id = this.activatedRoute.snapshot.params["id"];
-    this.changeComponentToEdit();
+    this.form.patchValue({
+      id: this.activatedRoute.snapshot.params["id"]
+    });
+    this.loadUserData();
   }
 
   onSubmit() {
@@ -44,34 +52,55 @@ export class CreateuserComponent implements OnInit {
 
   createUser() {
     this.api.callApi('api/v1/user', ApiMethods.POST, true, new Map(), this.form.value)
-      .subscribe(() => this.router.navigate(['users/']));
+      .subscribe(() => {
+        this.router.navigate(['users/']);
+        this.toastService.showSuccessToast('Creación Exitosa', `Usuario creado correctamente`);
+      });
   }
 
   updateUser() {
     this.api.callApi(`api/v1/user/${this.form.value.id}`, ApiMethods.PUT, true, new Map(), this.form.value)
-      .subscribe(() => this.router.navigate(['users/']));
-  }
-
-  changeComponentToEdit() {
-    if (this.id != undefined) {
-      this.title = "Editar Usuario"
-      this.loadUserData();
-    };
+      .subscribe(() => {
+        this.router.navigate(['users/']);
+        this.toastService.showSuccessToast('Actualización Exitosa', `Usuario editado correctamente`);
+      });
   }
 
   loadUserData() {
-    this.api.callApi(`api/v1/user/${this.id}`, ApiMethods.GET, true, new Map())
-      .subscribe((data: any) => {
-        this.form.setValue({
-          id: data.id,
-          name: data.name,
-          lastName: data.lastName,
-          email: data.email,
-          role: data.role,
-          pass: '',
-          confirmPass: '',
-          status: data.status
+    if (this.activatedRoute.snapshot.url[0].path == 'myuser') {
+      this.api.callApi('api/v1/user', ApiMethods.GET, true, new Map())
+        .subscribe((data: any) => {
+          this.title = "Editar Usuario"
+          this.form.setValue({
+            id: data.id,
+            name: data.name,
+            lastName: data.lastName,
+            email: data.email,
+            role: data.role,
+            pass: '',
+            confirmPass: '',
+            status: data.status
+          })
         });
-      });
+    } else if (this.activatedRoute.snapshot.url[0].path == 'edit') {
+      this.title = "Editar Usuario"
+      this.api.callApi(`api/v1/user/${this.id}`, ApiMethods.GET, true, new Map())
+        .subscribe((data: any) => {
+          this.form.setValue({
+            id: data.id,
+            name: data.name,
+            lastName: data.lastName,
+            email: data.email,
+            role: data.role,
+            pass: '',
+            confirmPass: '',
+            status: data.status
+          });
+        });
+    }
+  }
+
+  get userId() {
+    return this.form.get('id')
   }
 }
